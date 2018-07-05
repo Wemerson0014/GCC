a0158ef8773b588032d0bc00c7fcaf1e5a58bdf7
Wemerson0014 <wemersonf.rangel@outlook.com>
2018-07-04 22:13:03 -0300

    Criado views e models da aplicação

--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('curriculos.urls')),
+    path('', include('curriculos.urls')),
     path('admin/', admin.site.urls),
 ]
