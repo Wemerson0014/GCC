2f8e51b218c896d8ef8e55f4085ec2978b0ec742
Wemerson0014 <wemersonf.rangel@outlook.com>
2018-07-04 22:29:05 -0300

    Alterado aquivo de models

--- proj/curriculos/models.py
+++ proj/curriculos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Candidato(models.Model):
+    nome = models.CharField(max_length=50),
+    sobrenome = models.CharField(max_length=50),
+    biografia = models.TextField(),
+    pretensao_Salarial = models.DecimalField(max_digits=5, decimal_places=2),
+    observacao = models.TextField()
+    cargo = models.TextField()
+
