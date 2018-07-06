dcb2e6594927b228eaa6466810e07af334bf0ba6
Wemerson0014 <wemersonf.rangel@outlook.com>
2018-07-05 21:47:22 -0300

    Criado listagem de candidatos cadastrados

--- proj/curriculos/models.py
+++ proj/curriculos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Candidato(models.Model):
-    nome = models.CharField(max_length=50),
-    sobrenome = models.CharField(max_length=50),
-    biografia = models.TextField(),
-    pretensao_Salarial = models.DecimalField(max_digits=5, decimal_places=2),
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    biografia = models.TextField()
+    pretensao_Salarial = models.DecimalField(max_digits=5, decimal_places=2)
     observacao = models.TextField()
     cargo = models.TextField()
 
