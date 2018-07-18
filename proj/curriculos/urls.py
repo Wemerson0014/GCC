30599b344779a6cfde07912b9db1c88009e6f337
Wemerson0014 <wemersonf.rangel@outlook.com>
2018-07-17 22:23:27 -0300

    Arrumado erro de views durante a listagem de cadastros. Erro no arquivo de candidato-form.html na linha abaixo do IF.

--- proj/curriculos/urls.py
+++ proj/curriculos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import listagem, novo
+from .views import listagem, novo, atualizar, deletar
 
 urlpatterns = [
     path('', listagem, name='listagem_curriculos'),
-    path('novo', novo, name='novo_curriculos'),
+    path('novo/', novo, name='novo_curriculos'),
+    path('atualizar/<int:id>/', atualizar, name='atualizar_curriculos'),
+    path('deletar/<int:id>/', deletar, name='deletar_curriculos'),
 ]
--- proj/curriculos/views.py
+++ proj/curriculos/views.py
@@ -2,17 +2,40 @@ from django.shortcuts import render, redirect
 from .models import candidato
 from .forms import CandidatoForm
 
-def listagem (request):
+def listagem(request):
     candidatos = candidato.objects.all()
-    return render(request, 'curriculos.html', {'candidatos': candidato})
+    return render(request, 'curriculos.html', {'candidatos': candidatos})
 
-def novo (request):
+
+def novo(request):
     form = CandidatoForm(request.POST or None)
 
     if form.is_valid():
         form.save()
-        return redirect('listagem')
+        return redirect('listagem_curriculos')
 
     return render(request, 'candidato-form.html', {'form': form})
+
+
+def atualizar(request, id):
+    candidatos = candidato.objects.get(id=id)
+    form = CandidatoForm(request.POST or None, instance=candidato)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listagem_curriculos')
+
+    return render(request, 'candidato-form.html', {'form': form, 'candidato': candidato})
+
+
+def deletar(request, id):
+    candidatos = candidato.objects.get(id=id)
+
+    if request.method == 'POST':
+        candidatos.delete()
+        return redirect('listagem_curriculos')
+
+    return render(request, 'candidato-deletar.html', {'candidato': candidato})
+
 
 
